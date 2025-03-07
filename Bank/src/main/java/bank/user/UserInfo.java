8744c67d92e654172841d2c15702c2a08d12cb05
MohammadAli <m.alii.salek@gmail.com>
2025-03-07 13:54:40 +0330

    complete User and UserInfo classes

--- Bank/src/main/java/bank/user/UserInfo.java
+++ Bank/src/main/java/bank/user/UserInfo.java
@@ -1,4 +1,31 @@
 package bank.user;
 
-public class UserInfo {
+import bank.user.transaction.History;
+
+public class UserInfo extends User {
+
+    private double balance;
+    private History history;
+
+    public UserInfo(String id, String password, double balance, History history) {
+        super(id, password);
+        this.balance = balance;
+        this.history = history;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public History getHistory() {
+        return history;
+    }
+
+    public void setHistory(History history) {
+        this.history = history;
+    }
 }
