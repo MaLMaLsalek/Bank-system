279f5b4be99a99a6cc084fcf6333b4a0d36fa0d1
MohammadAli <m.alii.salek@gmail.com>
2025-03-07 13:41:15 +0330

    make User class and complete DataBase class

--- Bank/src/main/java/bank/data/DataBase.java
+++ Bank/src/main/java/bank/data/DataBase.java
@@ -1,4 +1,31 @@
 package bank.data;
 
+import bank.user.User;
+import bank.user.UserInfo;
+
+import java.util.ArrayList;
+
 public class DataBase {
+
+    ArrayList<User> user = new ArrayList<>();
+
+    ArrayList<UserInfo> userInfos = new ArrayList<>();
+
+    public void addUser(User x){
+        user.add(x);
+    }
+
+    public void addUserInfos(UserInfo x){
+        userInfos.add(x);
+    }
+
+    public boolean isUserExist(User x){
+        for (int i = 0 ; i < user.size() ; i ++){
+            if (x.equals(user.get(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
