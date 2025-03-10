bbec1c13aac388ac3e4b088ab15f75bc15363a3a
MohammadAli <m.alii.salek@gmail.com>
2025-03-10 10:39:24 +0330

    make Historylsit for user in History class

--- Bank/src/main/java/bank/user/History.java
+++ Bank/src/main/java/bank/user/History.java
@@ -1,4 +1,12 @@
 package bank.user;
 
+import java.util.ArrayList;
+
 public class History {
+
+    ArrayList<String> historylsit = new ArrayList<>();
+
+    public History(ArrayList<String> historylsit) {
+        this.historylsit = historylsit;
+    }
 }
