f6f20c4850419f7e1d050551c3c25721b6750ad0
MohammadAli <m.alii.salek@gmail.com>
2025-03-17 11:26:32 +0330

    make BankUser class for better development and use inheritance in User class

--- Bank/src/main/java/bank/bankuser/User.java
+++ Bank/src/main/java/bank/bankuser/User.java
@@ -2,27 +2,16 @@ package bank.bankuser;
 
 import java.util.ArrayList;
 
-public class User {
-    private String id;
+public class User extends BankUser {
     private long accountnum;
-    private String password;
     private double balance;
     private ArrayList<String> history;
 
     public User(String id, long accountnum, String password, double balance, ArrayList<String> History) {
-        this.id = id;
+        super(id, password);
         this.accountnum = accountnum;
-        this.password = password;
         this.balance = balance;
         this.history = History;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public long getAccountnum() {
@@ -31,14 +20,6 @@ public class User {
 
     public void setAccountnum(long accountnum) {
         this.accountnum = accountnum;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public double getBalance() {
