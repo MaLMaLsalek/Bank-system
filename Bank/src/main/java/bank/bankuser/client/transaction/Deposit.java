ee2002e48f6babdc85f6d33a014f111320eb759b
MohammadAli <m.alii.salek@gmail.com>
2025-03-17 14:00:43 +0330

    make Deposit and Withdrawal method

--- Bank/src/main/java/bank/bankuser/client/transaction/Deposit.java
+++ Bank/src/main/java/bank/bankuser/client/transaction/Deposit.java
@@ -1,4 +1,8 @@
 package bank.bankuser.client.transaction;
 
 public class Deposit {
+
+    public double depositmoney(double balance, double money) {
+        return balance + money;
+    }
 }
--- Bank/src/main/java/bank/bankuser/client/transaction/Withdrawal.java
+++ Bank/src/main/java/bank/bankuser/client/transaction/Withdrawal.java
@@ -1,4 +1,13 @@
 package bank.bankuser.client.transaction;
 
 public class Withdrawal {
+
+    public double withdrawalmoney(double balance, double drawalmoney) {
+        if (balance - drawalmoney >= 0){
+            return balance - drawalmoney;
+        }else {
+            System.out.println("There is not enough inventory");
+            return balance;
+        }
+    }
 }
