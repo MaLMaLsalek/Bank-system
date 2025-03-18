ebfb7141ded6ec91bcee4a5a5dbe7a7d8119cbd2
MohammadAli <m.alii.salek@gmail.com>
2025-03-18 20:43:36 +0330

    make transmissionMonyTo method for user to transmission mony

--- Bank/src/main/java/bank/bankuser/client/transaction/Transmission.java
+++ Bank/src/main/java/bank/bankuser/client/transaction/Transmission.java
@@ -1,9 +1,17 @@
 package bank.bankuser.client.transaction;
 
+import bank.data.DataBase;
+
 public class Transmission {
 
-    public void transmissionMonyTo(double balance, double money, long accountnum , int accountnumindex) {
+    public void transmissionMonyTo(double balance, double money, long accountnum) {
+        double x = DataBase.getINSTANCE().getUser().get(DataBase.getINSTANCE().findAccountIndex(accountnum)).getBalance();
+        if (balance - money >= 0) {
+            DataBase.getINSTANCE().getUser().get(DataBase.getINSTANCE().findAccountIndex(accountnum)).setBalance(x + money);
+        } else {
+            System.out.println("There is not enough money");
 
+        }
     }
 
 }
